204301aa48afabc3864f555fa68ce646d4fde280
Jamprey25 <53832941+Jamprey25@users.noreply.github.com>
2025-05-20 12:06:52 -0400

    Initial Commit

---
+++ SeniorProject/SeniorProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeniorProjectApp.swift
+//  SeniorProject
+//
+//  Created by Joseph Amprey on 5/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SeniorProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
