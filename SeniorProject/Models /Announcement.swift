786e82f0bc9f1fddd8411e3ac609edca95cceffe
Jamprey25 <53832941+Jamprey25@users.noreply.github.com>
2025-05-20 22:45:45 -0400

    Created announcement Struct

--- SeniorProject/Models /Announcement.swift
+++ SeniorProject/Models /Announcement.swift
@@ -6,4 +6,44 @@
 //
 
 import Foundation
+struct Announcement: Identifiable {
+    let id: UUID
+    let clubID: UUID
+    let authorID: UUID
+    let title: String
+    let content: String
+    let creationDate: Date
+    //let imageURLs: [URL]?
+    let isPinned: Bool
+    var reactions: [UUID: ReactionType]
     
+    enum ReactionType: String, Codable, CaseIterable {
+        case like = "👍"
+        case love = "❤️"
+        case laugh = "😂"
+        case wow = "😮"
+        case sad = "😢"
+    }
+  
+    init(id: UUID = UUID(),
+             clubID: UUID,
+             authorID: UUID,
+             title: String,
+             content: String,
+             creationDate: Date = Date(),
+             isPinned: Bool = false,
+             reactions: [UUID: ReactionType] = [:]) {
+            self.id = id
+            self.clubID = clubID
+            self.authorID = authorID
+            self.title = title
+            self.content = content
+            self.creationDate = creationDate
+            self.isPinned = isPinned
+            self.reactions = reactions
+        }
+    
+    
+    
+    
+}
