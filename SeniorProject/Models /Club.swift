79dbdb7d8dcfe42fb1699c772ad376d92c7db1aa
Jamprey25 <53832941+Jamprey25@users.noreply.github.com>
2025-05-20 22:37:56 -0400

    Created user class + added model files
    intialized the user clss
    created the files for the other models I will need

---
+++ SeniorProject/Models /Club.swift
@@ -0,0 +1,9 @@
+//
+//  Club.swift
+//  SeniorProject
+//
+//  Created by Joseph Amprey on 5/20/25.
+//
+
+import Foundation
+    
---
+++ SeniorProject/Models /Event.swift
@@ -0,0 +1,8 @@
+//
+//  Event.swift
+//  SeniorProject
+//
+//  Created by Joseph Amprey on 5/20/25.
+//
+
+import Foundation
