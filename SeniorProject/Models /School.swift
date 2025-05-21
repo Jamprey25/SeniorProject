645d9fc52225d6234bbef66b80ca78e385e83349
Jamprey25 <53832941+Jamprey25@users.noreply.github.com>
2025-05-20 23:13:49 -0400

    created school struct

--- SeniorProject/Models /School.swift
+++ SeniorProject/Models /School.swift
@@ -4,5 +4,29 @@
 //
 //  Created by Joseph Amprey on 5/20/25.
 //
-
 import Foundation
+struct School:  Identifiable {
+    let id: UUID
+    var name: String
+    let domain: String
+    //var logoURL: URL?
+    var administratorIDs: [UUID]
+    var clubIDs: [UUID]
+    init(id: UUID = UUID(),
+         name: String,
+         domain: String,
+         administratorIDs: [UUID] = [],
+         clubIDs: [UUID] = []) {
+        self.id = id
+            self.name = name
+            self.domain = domain
+            self.administratorIDs = administratorIDs
+            self.clubIDs = clubIDs
+        }
+    
+    
+    
+    
+    
+    
+}
